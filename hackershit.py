69c62edf1f527001b956fc2f1e1dfc3ab8424cec
Glen Dorning <50457485+Glengend@users.noreply.github.com>
2020-04-15 14:22:32 +0200

    Initial commit

---
+++ hackershit.py
@@ -0,0 +1,12 @@
+for a in range(0, 10):
+    for b in range(0, 9):
+        for c in range(0 , 8):
+            for d in range(0 , 7):
+
+                print(str(a), end=' ')
+                print(str(b), end =' ')
+                print(str(c), end =' ')
+
+
+
+        
